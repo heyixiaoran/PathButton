79be81e629cf5853210eaf141a62125ba6ec0e4b
heyixiaoran <heyixiaoran@vip.qq.com>
2016-04-01 15:48:42 +0800

    first commit

---
+++ PathButton/DelegateCommand.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Windows.Input;
+
+namespace PathButton
+{
+    public class DelegateCommand : ICommand
+    {
+        #region ICommand 成员
+
+        public bool CanExecute(object parameter)
+        {
+            if (CanExecuteFunc == null)
+            {
+                return true;
+            }
+
+            return CanExecuteFunc(parameter);
+        }
+
+        public event EventHandler CanExecuteChanged;
+
+        public void Execute(object parameter)
+        {
+            if (ExecuteAction == null)
+            {
+                return;
+            }
+
+            ExecuteAction(parameter);
+        }
+
+        #endregion
+
+        public Action<object> ExecuteAction { get; set; }
+        public Func<object, bool> CanExecuteFunc { get; set; }
+    }
+
+}
---
+++ PathButton/PathButton.xaml.cs
@@ -0,0 +1,68 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace PathButton
+{
+    /// <summary>
+    /// Interaction logic for UserControl1.xaml
+    /// </summary>
+    public partial class PathButton : Button
+    {
+        public Geometry PathData
+        {
+            get { return (Geometry)GetValue(PathDataProperty); }
+            set { SetValue(PathDataProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for PathData.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty PathDataProperty =
+            DependencyProperty.Register("PathData", typeof(Geometry), typeof(PathButton), new PropertyMetadata(new PathGeometry()));
+
+        public Brush DefaultFillBrush
+        {
+            get { return (Brush)GetValue(DefaultFillBrushProperty); }
+            set { SetValue(DefaultFillBrushProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for DefaultFillBrush.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty DefaultFillBrushProperty =
+            DependencyProperty.Register("DefaultFillBrush", typeof(Brush), typeof(PathButton), new PropertyMetadata(Brushes.DarkGray));
+
+        public Brush MouseOverBrush
+        {
+            get { return (Brush)GetValue(MouseOverBrushProperty); }
+            set { SetValue(MouseOverBrushProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for MouseOverBrush.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty MouseOverBrushProperty =
+            DependencyProperty.Register("MouseOverBrush", typeof(Brush), typeof(PathButton), new PropertyMetadata(Brushes.DeepSkyBlue));
+
+        public Brush IsPressedBrush
+        {
+            get { return (Brush)GetValue(IsPressedBrushProperty); }
+            set { SetValue(IsPressedBrushProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for IsPressedBrush.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty IsPressedBrushProperty =
+            DependencyProperty.Register("IsPressedBrush", typeof(Brush), typeof(PathButton), new PropertyMetadata(Brushes.DodgerBlue));
+
+        public Brush IsEnabledBrush
+        {
+            get { return (Brush)GetValue(IsEnabledBrushProperty); }
+            set { SetValue(IsEnabledBrushProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for IsEnabledBrush.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty IsEnabledBrushProperty =
+            DependencyProperty.Register("IsEnabledBrush", typeof(Brush), typeof(PathButton), new PropertyMetadata(Brushes.LightGray));
+
+        public PathButton()
+        {
+            InitializeComponent();
+            DataContext = this;
+        }
+    }
+}
